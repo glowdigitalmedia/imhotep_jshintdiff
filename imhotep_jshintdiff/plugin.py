4f0585c8969003f35ce8dce86ffc3dfcccfb9e0e
Konrad Korzel <konrad@korzel.com>
2014-11-14 18:01:13 +0000

    Fix parsing jshint output

--- imhotep_jshintdiff/plugin.py
+++ imhotep_jshintdiff/plugin.py
@@ -1,4 +1,4 @@
-import os
+import re
 
 from imhotep.tools import Tool
 
@@ -10,12 +10,17 @@ class JSHintDiff(Tool):
         self.linter = 'jshint'
         self.config = '.jscsrc'
         self.extension = 'js'
+        self.regex = re.compile(
+            r'(?P<filename>.*): line (?P<line_num>\d+), col \d+, (?P<message>.*)'
+        )
 
     def format_linter_output(self, repo_dir, linter_output, linter_errors):
         """ Returns a dict of linter errors """
         for line in linter_output:
-            path, line, column, message = line.split(':')
-            file_name = os.path.abspath(path)
-            file_name = file_name.replace(repo_dir, '')[1:]
-            linter_errors[str(file_name)][line].append(message.lstrip(' '))
+            match = self.regex.search(line)
+            if match is not None:
+                filename = match.group('filename')[len(repo_dir) + 1:]
+                linter_errors[filename][match.group('line_num')].append(
+                    match.group('message')
+                )
         return linter_errors
